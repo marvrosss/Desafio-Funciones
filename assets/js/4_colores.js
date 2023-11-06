ecdbb01cb01dcfdda4663ddea3fc8211027c1244
marvrosss <marvrosss@gmail.com>
2023-11-06 19:01:13 -0300

    3r commit

--- assets/js/4_colores.js
+++ assets/js/4_colores.js
@@ -1,19 +1,27 @@
-function pintar1(color) {
-    elemento = document.getElementById("b1");
-    elemento.style.backgroundColor = color;
-    }
+function pintar1() {
+    let div = document.getElementById("b1");
+    div.style.backgroundColor = 'black';
+  }
+    let div1 = document.getElementById("b1");
+    div1.addEventListener("click", pintar1);
 
-function pintar2(color) {
-    elemento = document.getElementById("b2");
-    elemento.style.backgroundColor = color;
-    }
+function pintar2() {
+    let div = document.getElementById("b2");
+    div.style.backgroundColor = 'black';
+  }
+    let div2 = document.getElementById("b2");
+    div1.addEventListener("click", pintar2);
 
-function pintar3(color) {
-    elemento = document.getElementById("b3");
-    elemento.style.backgroundColor = color;
-    }
+function pintar3() {
+    let div = document.getElementById("b3");
+    div.style.backgroundColor = 'black';
+  }
+    let div3 = document.getElementById("b3");
+    div3.addEventListener("click", pintar3);
 
-function pintar4(color) {
-    elemento = document.getElementById("b4");
-    elemento.style.backgroundColor = color;
-    }
+function pintar4() {
+    let div = document.getElementById("b4");
+    div.style.backgroundColor = 'black';
+  }
+    let div4 = document.getElementById("b4");
+    div1.addEventListener("click", pintar4);
