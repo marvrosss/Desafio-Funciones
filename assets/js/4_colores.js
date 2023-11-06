3f45fc5864cc71f85301247012c771685e26bfa3
marvrosss <marvrosss@gmail.com>
2023-11-06 18:45:30 -0300

    segundo

--- assets/js/4_colores.js
+++ assets/js/4_colores.js
@@ -1,5 +1,19 @@
-function pintar(){
-ele.style.backgroundColor = 'black'
-}
-ele = document.getElementById("b1")
-ele.addEventListener("click", pintar);
+function pintar1(color) {
+    elemento = document.getElementById("b1");
+    elemento.style.backgroundColor = color;
+    }
+
+function pintar2(color) {
+    elemento = document.getElementById("b2");
+    elemento.style.backgroundColor = color;
+    }
+
+function pintar3(color) {
+    elemento = document.getElementById("b3");
+    elemento.style.backgroundColor = color;
+    }
+
+function pintar4(color) {
+    elemento = document.getElementById("b4");
+    elemento.style.backgroundColor = color;
+    }
